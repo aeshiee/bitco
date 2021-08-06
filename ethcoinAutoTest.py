0779c914cbfc45dd24bd60b56d31947ed07e2cbe
aeshiee <88122666+aeshiee@users.noreply.github.com>
2021-08-06 23:39:29 +0900

    Update ethcoinAutoTest.py

--- ethcoinAutoTest.py
+++ ethcoinAutoTest.py
@@ -42,22 +42,18 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time("KRW-ETH")
         end_time = start_time + datetime.timedelta(days=1)
-         
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):  
-            target_price = get_target_price("KRW-ETH", 0.2)
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-ETH", 0.3)
             current_price = get_current_price("KRW-ETH")
-            if target_price < current_price : 
-                if  upbit.get_avg_buy_price("KRW-BTC")> 0 : 
-                    krw = get_balance("KRW")  
-                    upbit.buy_market_order("KRW-ETH",krw*0.9995) 
-
-            if upbit.get_avg_buy_price("KRW-ETH")  > 0 :  
-                balance_sell_value =  ( pyupbit.get_current_price("KRW-ETH") - upbit.get_avg_buy_price("KRW-ETH")  ) / upbit.get_avg_buy_price("KRW-ETH") * 100  
-                if balance_sell_value > 1.4 : 
-                    upbit.sell_market_order("KRW-ETH", get_balance("ETH")*0.9995 )
-                
- 
+            if target_price < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-ETH", krw*0.9995)
+        else:
+            btc = get_balance("ETH")
+            if btc > 0.00008:
+                upbit.sell_market_order("KRW-ETH", btc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
